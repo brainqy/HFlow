47bea5fe61cdecf87ab6906b5d4ff860046d8523
Dnyanesh <somwanshi.dnyaneshwar@gmail.com>
2025-06-06 23:15:18 +0000

    avoid overlap

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -362,7 +362,7 @@ const SidebarHeader = React.forwardRef<
     <div
       ref={ref}
       data-sidebar="header"
-      className={cn("flex flex-col gap-2 p-2", className)}
+      className={cn("flex flex-col gap-2 p-2 shrink-0", className)}
       {...props}
     />
   )
@@ -377,7 +377,7 @@ const SidebarFooter = React.forwardRef<
     <div
       ref={ref}
       data-sidebar="footer"
-      className={cn("flex flex-col gap-2 p-2", className)}
+      className={cn("flex flex-col gap-2 p-2 shrink-0", className)}
       {...props}
     />
   )
